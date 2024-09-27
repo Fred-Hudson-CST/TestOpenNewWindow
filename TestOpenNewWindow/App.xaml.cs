6f0ad1f4dcecc45a831e648496871379a3b69261
Fred Hudson <Fhudson@ControlSystems.com.au>
2024-09-27 11:30:34 +1000

    Add project files.

---
+++ TestOpenNewWindow/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace TestOpenNewWindow
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+           
+        }
+        
+    }
+}
---
+++ TestOpenNewWindow/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestOpenNewWindow
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
